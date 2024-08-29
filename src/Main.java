61393e3d436af08740ca94b5e38e66a0df65279f
jasper <whiteana001@warren.k12.in.us>
2024-08-29 13:22:22 -0400

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(isCatPlaying(true, 10));
+        System.out.println(isCatPlaying(false, 36));
+        System.out.println(isCatPlaying(false, 35));
+    }
+
+    private static boolean isCatPlaying(boolean summer, int temp) {
+        if(summer){
+            return temp >= 25 && temp <= 45;
+        } else {
+            return temp >= 25 && temp <= 35;
+        }
+    }
+
 }
